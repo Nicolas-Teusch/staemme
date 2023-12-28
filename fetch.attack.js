b2c07d026b93c5ac04793e034e7e9e92280732ad
Nicolas-Teusch <nicolas_teusch@web.de>
2023-12-28 21:14:15 +0100

    fetch removed

---
+++ fetch.attack.js
@@ -0,0 +1,23 @@
+
+
+fetch("https://des1.die-staemme.de/game.php?village=1326&screen=place&ajaxaction=popup_command", {
+  "headers": {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-language": "en,de-DE;q=0.9,de;q=0.8,en-DE;q=0.7,ru-RU;q=0.6,ru;q=0.5,en-US;q=0.4",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "tribalwars-ajax": "1",
+    "x-requested-with": "XMLHttpRequest"
+  },
+  "referrer": "https://des1.die-staemme.de/game.php?village=1326&screen=map",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "attack=true&ch=cf97796acf627ce342928fd98d396ec2%3A5865f2a6dedad9bac5e5ec96fd5aa8a59a3b72986c294eda59604d3a9cc3b6f4&cb=troop_confirm_submit&x=463&y=503&source_village=1326&village=1326&attack_name=&spear=44&sword=28&axe=0&spy=0&light=0&heavy=0&ram=0&catapult=0&knight=0&snob=0&building=main&h=ca7b3bd6&h=ca7b3bd6",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});

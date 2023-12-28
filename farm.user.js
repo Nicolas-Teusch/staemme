49f6977dc03fd2325d2c460347016c71f8ddfb09
Nicolas-Teusch <nicolas_teusch@web.de>
2023-12-28 21:07:22 +0100

    url match

--- farm.user.js
+++ farm.user.js
@@ -4,7 +4,7 @@
 // @version      1.2.2
 // @description  Fams selected villages.
 // @author       You
-// @match        https://*.die-staemme.de/game.php?village=*&screen=main
+// @match        https://*.die-staemme.de/game.php*screen=map*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=die-staemme.de
 // @grant        none
 // ==/UserScript==

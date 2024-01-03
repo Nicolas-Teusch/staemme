047085362254f51be2661b999cf19bad50c0ee80
Nicolas-Teusch <nicolas_teusch@web.de>
2024-01-03 17:17:23 +0100

    check if troops are available v2

--- farm.user.js
+++ farm.user.js
@@ -20,138 +20,128 @@ TWMap.villages
 
 
 */
+
+const contentValue = document.querySelector('#content_value');
+
+
+let ptr = 0;
+let interval = null;
+
+function getNextTarget() {
+
+    let TargetBBCodes = targets.value.split('\n').filter((line) => line !== '');
+    if (TargetBBCodes.length === 0) {
+        return null;
+    }
+
+    ptr = ptr % TargetBBCodes.length;
+
+    const BBCode = TargetBBCodes[ptr];
+
+    ptr++;
+
+    return BBCode;
+}
+
+function getSelectedTemplate() {
+    return selectTemplate.value
+}
+
+
+function checkIfTroopsAreAvailable() {
+    /*
+        {
+            "spear": "1287",
+            "sword": "213",
+            "axe": "0",
+            "archer": "0",
+            "spy": "0",
+            "light": "0",
+            "marcher": "0",
+            "heavy": "50",
+            "ram": "0",
+            "catapult": "0",
+            "knight": "1",
+            "snob": "0"
+        }
+    */
+    const currentUnits = TWMap.current_units
+    const selectedTemplateId = getSelectedTemplate();
+    const selectedTemplate = TroopTemplates.current[selectedTemplateId]
+    for(const [k,v] of Object.entries(selectedTemplate)) {
+        if(currentUnits[k] < v || $(`#units_entry_all_${k}`).innerHTML.match(/\d+/)[0] < v) {
+            return false;
+        }
+    }
+
+    
+
+    return true
+
+
+}
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
+async function startFarming() {
+    console.log("start farming");
+    interval = setInterval(async () => {
+
+        console.log("checking for next target");
+        const target = getNextTarget();
+        console.log("next target", target);
+
+        if (target === null || target === '') {
+            return;
+        }
+
+        // target village is passed like x|y
+        // village ids are stored in TWMAP.villages where xy is key
+        // id like "32310"
+        let xy = target.match(/\d+\|\d+/)[0].split('|').join('');
+        let e = TWMap.villages[xy].id;
+        CommandPopup.openRallyPoint({
+            target: e
+        })
+     
+
+        while (!checkIfTroopsAreAvailable()) {
+            console.log("waiting for troops to be available");
+            await sleep(1000);
+        }
+
+        // popup should open
+        // now select template
+        let template = getSelectedTemplate();
+        TroopTemplates.useTemplate(template);
+
+
+        document.getElementById('target_attack').click();
+        
+     
+        while (!document.getElementById('troop_confirm_submit')) {
+            console.log("waiting for troop_confirm_submit");
+            await sleep(1000);
+        }
+
+        document.getElementById('troop_confirm_submit').click();
+
+
+
+    }, 3000);
+}
+
+function stopFarming() {
+    if (interval !== null) {
+        clearInterval(interval);
+    }
+
+}
+
 (function () {
     'use strict';
 
-    const contentValue = document.querySelector('#content_value');
 
-
-    let ptr = 0;
-    let interval = null;
-
-    function getNextTarget() {
-
-        let TargetBBCodes = targets.value.split('\n').filter((line) => line !== '');
-        if (TargetBBCodes.length === 0) {
-            return null;
-        }
-
-        ptr = ptr % TargetBBCodes.length;
-
-        const BBCode = TargetBBCodes[ptr];
-
-        ptr++;
-
-        return BBCode;
-    }
-
-    function getSelectedTemplate() {
-        return selectTemplate.value
-    }
-
-
-    function checkIfTroopsAreAvailable() {
-        /*
-            {
-                "spear": "1287",
-                "sword": "213",
-                "axe": "0",
-                "archer": "0",
-                "spy": "0",
-                "light": "0",
-                "marcher": "0",
-                "heavy": "50",
-                "ram": "0",
-                "catapult": "0",
-                "knight": "1",
-                "snob": "0"
-            }
-        */
-        const currentUnits = TWMap.current_units
-        const selectedTemplateId = getSelectedTemplate();
-        const selectedTemplate = TroopTemplates.current[selectedTemplateId]
-        for(const [k,v] of Object.entries(selectedTemplate)) {
-            if(currentUnits[k] < v) {
-                return false;
-            }
-        }
-
-        return true
-
-
-    }
-
-
-    function attackVillage(targetVillage, template) {
-        const villageId = game_data.village.id;
-        const world = game_data.world;
-
-        //const url = `https://${world}.die-staemme.de/game.php?village=${villageId}&screen=place&ajaxaction=popup_command`;
-        const url = `https://${world}.die-staemme.de/game.php?t=9225854&village=${villageId}&screen=place&x=511&y=483&spear=0&sword=0&axe=0&archer=0&spy=0&light=0&marcher=0&heavy=0&ram=0&catapult=0&knight=1&snob=0`
-
-        //https://des1.die-staemme.de/game.php?&village=373&screen=place&x=511&y=483&spear=0&sword=0&axe=0&archer=0&spy=0&light=0&marcher=0&heavy=0&ram=0&catapult=0&knight=1&snob=0
-        //https://dec1.die-staemme.de/game.php?t=9225854&village=19143&screen=place&target=47995&spear=0&sword=0&axe=0&archer=0&spy=0&light=0&marcher=0&heavy=0&ram=0&catapult=0&knight=&snob=0
-    }
-
-
-
-
-    async function startFarming() {
-        console.log("start farming");
-        interval = setInterval(async () => {
-
-            console.log("checking for next target");
-            const target = getNextTarget();
-            console.log("next target", target);
-
-            if (target === null || target === '') {
-                return;
-            }
-
-            // target village is passed like x|y
-            // village ids are stored in TWMAP.villages where xy is key
-            // id like "32310"
-            let xy = target.match(/\d+\|\d+/)[0].split('|').join('');
-            let e = TWMap.villages[xy].id;
-            CommandPopup.openRallyPoint({
-                target: e
-            })
-         
-
-            while (!checkIfTroopsAreAvailable()) {
-                console.log("waiting for troops to be available");
-                await sleep(1000);
-            }
-
-            // popup should open
-            // now select template
-            let template = getSelectedTemplate();
-            TroopTemplates.useTemplate(template);
-
-
-            document.getElementById('target_attack').click();
-
-
-            const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
-         
-            while (!document.getElementById('troop_confirm_submit')) {
-                console.log("waiting for troop_confirm_submit");
-                await sleep(1000);
-            }
-
-            document.getElementById('troop_confirm_submit').click();
-
-
-
-        }, 3000);
-    }
-
-    function stopFarming() {
-        if (interval !== null) {
-            clearInterval(interval);
-        }
-
-    }
 
 
     const containerDiv = document.createElement('div');
